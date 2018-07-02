22b0ae3c6e86dda69e36aa553ffc697cf447187f
André Wendt <awendt@freeshell.org>
2018-07-02 11:50:10 +0200

    Add webpage
    - npx parcel watch index.html --out-dir website
    - then serve website/ through HTTP server

---
+++ index.js
@@ -0,0 +1,18 @@
+var mymap = L.map('mapid').setView([52.480, 13.454], 13);
+
+// Mapbox looks beautiful but requires an API token
+// see https://wiki.openstreetmap.org/wiki/Tiles
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
+  maxZoom: 18
+}).addTo(mymap);
+
+function renderGeoJSON() {
+  geojsonFeature = JSON.parse(this.responseText);
+  L.geoJSON(geojsonFeature).addTo(mymap);
+}
+
+var oReq = new XMLHttpRequest();
+oReq.addEventListener("load", renderGeoJSON);
+oReq.open("GET", "/all.json");
+oReq.send();
