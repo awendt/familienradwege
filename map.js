b7a421c6b59f5e117a1245aaee5d688f580fb14e
André Wendt <awendt@freeshell.org>
2018-08-17 16:53:32 +0200

    Change to upper case identifiers for constants

--- map.js
+++ map.js
@@ -1,12 +1,12 @@
 const BERLIN_COORDINATES = [52.480, 13.454];
 
-const geojson_options = {
+const GEOJSON_OPTIONS = {
   pointToLayer: () => {}
 }
 
 export const map = L.map('mapid').setView(BERLIN_COORDINATES, 12);
 
 export const layers = {
-  roads: new L.geoJSON(undefined, geojson_options),
-  manual: new L.geoJSON(undefined, geojson_options)
+  roads: new L.geoJSON(undefined, GEOJSON_OPTIONS),
+  manual: new L.geoJSON(undefined, GEOJSON_OPTIONS)
 };
