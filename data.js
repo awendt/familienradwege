970892cb3a6e57044b2b10368c3b64afe9ffa09e
André Wendt <awendt@freeshell.org>
2019-01-24 08:53:43 +0100

    [skip ci] Remove all website-related files

--- data.js
+++
@@ -1,8 +0,0 @@
-function load_json(file, cb) {
-  var oReq = new XMLHttpRequest();
-  oReq.addEventListener("load", cb);
-  oReq.open("GET", "/"+ file +".json");
-  oReq.send();
-}
-
-export { load_json };
